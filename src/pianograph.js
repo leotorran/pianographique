dea82d363cbbf2ce41c08e9c69be04b5e4d53464
Léo <rennard.leo.2008@gmail.com>
2021-11-21 21:54:32 +0100

    Update pianograph.js

--- src/pianograph.js
+++ src/pianograph.js
@@ -40,13 +40,35 @@ class pianograph extends Phaser.Scene {
         note1.setBounce(1, 1);
         note1.setCollideWorldBounds(true);
 
+        let note2 = this.physics.add.image(500, 200, 'note2');
+        note2.setVelocity(-100, 200);
+        note2.setBounce(1, 1);
+        note2.setCollideWorldBounds(true);
+
+        let note3 = this.physics.add.image(800, 200, 'note3');
+        note3.setVelocity(-100, 200);
+        note3.setBounce(1, 1);
+        note3.setCollideWorldBounds(true);
+
+        let dnote1 = this.physics.add.image(800, 200, 'dnote1');
+        dnote1.setVelocity(-20, 200);
+        dnote1.setBounce(1, 1);
+        dnote1.setCollideWorldBounds(true);
+
+        let sonA = scene.sound.add(sonA);
+
+
         initKeyboard()
         {
-            this.input.keyboard.on('keyA', function (kevent) {
+            let me = this;
+            this.input.keyboard.on('sonA', function (kevent) {
                 switch (kevent.keyCode) {
-                    soundA.play();
+                    case Phaser.Input.Keyboard.KeyCodes.LEFT:
+                        sonA.play;
+                        break;
                 }
             });
         }
+
     }
 }
